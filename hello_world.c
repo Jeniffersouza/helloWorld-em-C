8b2f946cfccc5631c843bc28724e46126d2dbb9a
Jeniffer souza <ajenifferbsouza@gmail.com>
2023-02-22 21:37:33 -0300

    code first

---
+++ hello_world.c
@@ -0,0 +1,21 @@
+#include<stdio.h> //biblioteca que é importada para rodar o sistema
+
+// Função que retorna um inteiro
+// printf("") - printa na tela 
+//const = constante 
+// int = inteiro
+// float = numero com ponto  
+// boolean = lógico true ou false
+// if = condição if else
+// for = repetição 
+// switch = repetição - loop
+
+int main() {
+
+    printf("Hello World!");
+    return 0;
+
+}
+
+// no terminal, usar gcc [nome do arquivo].c -Wall - Cria o exe.
+// no terminal digitar [nome do arquivo .exe] que compila o arquivo.
